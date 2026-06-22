84f41b96367728b5a10edc0c50b5fcf554617d77
coaltic <glindsay156@gmail.com>
2026-06-21 21:19:20 -0300

    Added Enemy Chasing
    Enemies will now chase the player when within a certain range, and go back to idling when out of range

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public float speed;
+    public bool isChasing;
+    private Rigidbody2D rb;
+    private Transform player;
+
+
+    void Start()
+    {
+        speed = 4;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChasing == true)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (player == null) player = collision.transform;
+            isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.linearVelocity = Vector2.zero;
+            isChasing = false;
+        }
+
+    }
+}
