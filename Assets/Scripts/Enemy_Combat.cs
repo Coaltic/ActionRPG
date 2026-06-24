16e739e28f5b9d43d55b6ef463fc72a92c2a09c2
coaltic <glindsay156@gmail.com>
2026-06-24 20:31:30 -0300

    Minor Code Changes
    Minor code changes, no new added features

--- Assets/Scripts/Enemy_Combat.cs
+++ Assets/Scripts/Enemy_Combat.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Enemy_Combat : MonoBehaviour
 {
 
+    public int health = 5;
     public int damage = 1;
     private void OnCollisionEnter2D(Collision2D collision)
     {
