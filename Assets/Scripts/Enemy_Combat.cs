eaa6148d04dbd12407db79ee5b539bac7740f46d
coaltic <glindsay156@gmail.com>
2026-06-25 17:25:29 -0300

    Goblins Now Deal Damage From Weapon
    Goblins now deal damage based off the collision with their weapon

--- Assets/Scripts/Enemy_Combat.cs
+++ Assets/Scripts/Enemy_Combat.cs
@@ -5,14 +5,24 @@ public class Enemy_Combat : MonoBehaviour
 
     public int health = 5;
     public int damage = 1;
+    public Transform attackPoint;
+    public float weaponRange;
+    public LayerMask playerLayer;
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if (collision.gameObject.tag == "Player") collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        // if (collision.gameObject.tag == "Player") collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
             
     }
 
     public void Attack()
     {
-        Debug.Log("attacked");
+        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, playerLayer);
+
+        if (hits.Length > 0)
+        {
+            hits[0].GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        }
     }
 }
--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -77,7 +77,7 @@ public class Enemy_Movement : MonoBehaviour
         }
     }*/
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
